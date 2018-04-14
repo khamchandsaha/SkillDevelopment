becc6624e08727fd55cd1c8348b36ada5988c31b
khamchand Saha <khamchand@nitsmirza.ac.in>
2018-04-14 15:46:03 +0530

    Working File Upload Servlet

---
+++ src/filemanagement/UploadServlet.java
@@ -0,0 +1,61 @@
+package filemanagement;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+
+ 
+/**
+ * @author khamchand
+ * This servlet is used to upload the files
+ */
+@WebServlet("/UploadServlet")
+@MultipartConfig
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Part filePart = request.getPart("file"); 
+	    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); 
+	    InputStream fileContent = filePart.getInputStream();
+	    File uploads = new File("D:/");
+	    File file = new File(uploads, fileName);
+
+	    try (InputStream input = filePart.getInputStream()) {
+	        Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+	    }
+	}
+
+}
